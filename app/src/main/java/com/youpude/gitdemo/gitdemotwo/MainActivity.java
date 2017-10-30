573e5f058983073012a551f05f146cccbee52178
yuanlulu <18280579017@163.com>
2017-10-30 14:05:43 +0800

    添加Toast功能

--- app/src/main/java/com/youpude/gitdemo/gitdemotwo/MainActivity.java
+++ app/src/main/java/com/youpude/gitdemo/gitdemotwo/MainActivity.java
@@ -2,6 +2,7 @@ package com.youpude.gitdemo.gitdemotwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "这添加Toast功能！", Toast.LENGTH_SHORT).show();
     }
 }
